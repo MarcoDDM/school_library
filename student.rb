a6fd398f7096e8bc7cbdaff5dd73e36fbffc573f
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-07-24 16:43:30 -0300

    Fixed files for linters

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name = 'Unknown', age = nil, parent_permission = true, classroom)
-    super(name, age, parent_permission)
+  def initialize(classroom:, name: 'Unknown', age: nil, parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(name = 'Unknown', age = nil, parent_permission = true, specialization)
-    super(name, age, parent_permission)
+  def initialize(specialization:, name: 'Unknown', age: nil, parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
