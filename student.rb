3bf438020c4d075104f958fee742d2163858e875
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-07-24 16:09:04 -0300

    Linters modification fixed

--- student.rb
+++ student.rb
@@ -3,12 +3,12 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name = "Unknown", age = nil, parent_permission = true, classroom)
+  def initialize(name = 'Unknown', age = nil, parent_permission = true, classroom)
     super(name, age, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\\(ツ)/¯"
+    '¯\\(ツ)/¯'
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(name = "Unknown", age = nil, parent_permission = true, specialization)
+  def initialize(name = 'Unknown', age = nil, parent_permission = true, specialization)
     super(name, age, parent_permission)
     @specialization = specialization
   end
