b00e58ac5f7238708a623026e5c879bd99f6b18a
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-07-24 19:08:47 -0300

    Linters check

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(name: 'Unknown', age: nil, parent_permission: true, specialization:)
+  def initialize(specialization:, name: 'Unknown', age: nil, parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
