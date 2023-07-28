e8bd2f5c97a4ec6c8f1a54b8e26a24cc8ab0bd2e
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-07-28 15:55:47 -0300

    Modified to fix linters errors

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require_relative 'rental'
 
 class App
   def initialize
-    @classroom = Classroom.new("Math Class")
+    @classroom = Classroom.new('Math Class')
     @people = []
     @books = []
     @rentals = []
@@ -21,9 +21,9 @@ class App
   end
 
   def create_person(role, name, age)
-    if role == "student"
+    if role == 'student'
       person = Student.new(classroom: @classroom, name: name, age: age)
-    elsif role == "teacher"
+    elsif role == 'teacher'
       person = Teacher.new(name: name, age: age)
     else
       puts "Invalid role. Please enter 'student' or 'teacher'."
@@ -45,13 +45,13 @@ class App
     book = @books.find { |b| b.title == book_title }
 
     if person && book
-      rental = Rental.new("2023-07-26", book, person)
+      rental = Rental.new('2023-07-26', book, person)
       person.add_rental(rental, book)
       book.add_rental(rental)
       @rentals << rental
-      puts "Rental created successfully!"
+      puts 'Rental created successfully!'
     else
-      puts "Person or book not found. Please check the IDs and titles and try again."
+      puts 'Person or book not found. Please check the IDs and titles and try again.'
     end
   end
 
@@ -64,59 +64,50 @@ class App
         puts "#{rental.book.title} (#{rental.date})"
       end
     else
-      puts "Person not found. Please check the ID and try again."
+      puts 'Person not found. Please check the ID and try again.'
     end
+  end
+
+  OPTIONS = {
+    1 => :list_all_books,
+    2 => :list_all_people,
+    3 => :create_person_choice,
+    4 => :create_book_choice,
+    5 => :create_rental_choice,
+    6 => :list_rentals_choice,
+    7 => :exit_app
+  }.freeze
+
+  def process_choice(choice)
+    action = OPTIONS[choice]
+
+    if action
+      send(action)
+    else
+      puts 'Invalid option. Please try again.'
+    end
+  end
+
+  def exit_app
+    puts 'Exiting the app...'
+    exit
   end
 
   def main
     loop do
       puts "\nWelcome to School Library App!"
       puts "\nPlease choose an option by entering a number:"
-      puts "1. List all books"
-      puts "2. List all people"
-      puts "3. Create a person"
-      puts "4. Create a book"
-      puts "5. Create a rental"
-      puts "6. List all rentals for a given id"
-      puts "7. Exit"
+      puts '1. List all books'
+      puts '2. List all people'
+      puts '3. Create a person'
+      puts '4. Create a book'
+      puts '5. Create a rental'
+      puts '6. List all rentals for a given id'
+      puts '7. Exit'
 
       choice = gets.chomp.to_i
 
-      case choice
-      when 1
-        list_all_books
-      when 2
-        list_all_people
-      when 3
-        puts "Enter role (student/teacher):"
-        role = gets.chomp.downcase
-        puts "Enter name:"
-        name = gets.chomp
-        puts "Enter age:"
-        age = gets.chomp.to_i
-        create_person(role, name, age)
-      when 4
-        puts "Enter title:"
-        title = gets.chomp
-        puts "Enter author:"
-        author = gets.chomp
-        create_book(title, author)
-      when 5
-        puts "Enter person ID:"
-        person_id = gets.chomp.to_i
-        puts "Enter book title:"
-        book_title = gets.chomp
-        create_rental(person_id, book_title)
-      when 6
-        puts "Enter person ID:"
-        person_id = gets.chomp.to_i
-        list_rentals_for_person(person_id)
-      when 7
-        puts "Exiting the app..."
-        break
-      else
-        puts "Invalid option. Please try again."
-      end
+      process_choice(choice)
     end
   end
 end
